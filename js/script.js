3e68c1275ddc087ecfa202fcf0199bd2c3cd239e
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-11-22 12:14:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Task indexOf()
-/*let array = [1,2,3,4,5];
+let array = [1,2,3,4,5,6,7,'d',76,34,'t','yu'];
 
-function lastIndexOf(arr, value) {
+function IndexOf(arr, value) {
     for (let i = 0; i <= arr.length -1; i++) {
       if (arr[i] === value) {
         return i;
@@ -9,23 +9,23 @@ function lastIndexOf(arr, value) {
     }
     return -1;
 }
-console.log (lastIndexOf(array,7))
+console.log (IndexOf(array,34))
 
 // Task lastIndexOf()
-/*let array = [1,2,3,4,5];
+let array = [1,2,3,4,5,6,7,'d',76,34,'t','yu'];
 
-function lastIndexOf(arr, value) {
-    for (let i = arr.length - 1; i >= 0; i--) {
+function lastIndexOf(arr, value, fromIndex = null) {
+  fromIndex === null ? fromIndex = arr.length -1 : null;
+    for (let i = fromIndex; i >= 0; i--) {
       if (arr[i] === value) {
         return i;
       }
     }
     return -1;
 }
-console.log (lastIndexOf(array,2))
-*/
+console.log (lastIndexOf(array,'t'));
+
 // Task find()
-/*let arr = [1,2,3,4,5];
 
 const find = function(arr,callback) {
   for (let i = 0;i <arr.length;i++) {
@@ -34,27 +34,83 @@ const find = function(arr,callback) {
     }
   }
 }
-console.log(find([1,2,3,3,3,4,5],item =>item === 3));
-*/
+console.log(find([1,2,3,23,33,4,5],item =>item === 4));
+
 
 // Task findIndex()
 
+const findIndex = function(arr,callback) {
+  for (let i = 0;i <arr.length;i++) {
+    if (callback(arr[i], i, arr)) {
+      return i
+    }
+  }
+}
+console.log(findIndex([1,2,3,23,33,4,5],item =>item === 4));
 
 // Task includes()
+const array = [1,2,3,4,5,6,7,'d',76,34,'t','yu']
 
+function includes(arr, value) {
+  for (let i = 0; i <= arr.length -1; i++) {
+    if (arr[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log (includes(array,35))
+
+
+// Решение нагуглил, я не понимаю как пользоваться функциями every и some.
+  //как задавать нужные параметры.
+
+
+
+// Task some()
+const array = [1,2,3,4,5,6,7,'d',76,34,'t','yu']
+
+let some = function(arr, callback, arg) {
+  let i, length = arr.length;
+  for (i = 0; i < length; i = i + 1) {
+    if (callback.call(arg, arr[i], i, arr)) {
+      return true;
+    }
+  }
+  return false;
+};
+console.log(some([1,2,3,4,5,6,7,'d',76,34,'t','yu'], item => item === 4));
 
 
 // Task every()
-let arr = [1,2,3,4,5];
-
-function every(a) {
-
-  for (let i = 0; i > arr.length; i++){
-    if (arr[i] > 0) {
-    return true;
-  } else {
-    return false;
+let every = function(arr, callback, arg) {
+  let i, length = arr.length;
+  for (i = 0; i < length; i = i + 1) {
+    if (!callback.call(arg, arr[i], i, arr)) {
+      return false;
+    }
   }
-  return console.log();
+  return true;
+};
+console.log(every([1,2,3,4,5,6,7,'d',76,34,'t','yu'], item => item === 4));
+
+
+
+
+
+// Это я для себя записал
+// task flat 
+/*let arr = [[1,2,3],[1,[2,3]],[[[[1,2,3,4]]]]];
+
+function flatFunc (paramArr, arrNew = []) {
+    for (let i = 0; i < paramArr.length; i++) {
+      if (Array.isArray(paramArr[i])) {
+          flatFunc(paramArr[i],arrNew);
+      } else {
+        arrNew.push(paramArr[i]);
+      }
+    }
+    return arrNew;
 }
-every(arr);
+console.log(flatFunc(arr));
+*/
